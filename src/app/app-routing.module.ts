5556b59ca51a346b4edbbdb3413669ac3a649292
Henrique C <henrique.cavicchioli@hotmail.com>
2022-07-15 16:00:04 -0300

    adicionando componente group-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GroupListComponent } from './group-list/group-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/groups',
+    component: GroupListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupListComponent } from './group-list/group-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupListComponent
   ],
   imports: [
     BrowserModule,
