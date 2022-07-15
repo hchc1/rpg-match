42e701d50ed87c4c0fd1127f869f5f708b69bb1d
Henrique C <henrique.cavicchioli@hotmail.com>
2022-07-15 17:23:39 -0300

    adicionando group model

--- src/app/group-list/group-list.component.ts
+++ src/app/group-list/group-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Group } from '../group.model';
 
 @Component({
   selector: 'app-group-list',
@@ -7,18 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupListComponent implements OnInit {
 
-  sessions = [
+  sessions: Group[] = [
+    new Group(
+    'Sessao top',
+    'D&D 5e',
+    'Aventura',
+    new Date('2022-07-16'),
+    ''
+    ),
     {
-    name: 'Sessao top',
+    title: 'Sessao FERAS',
     system: 'D&D 5e',
     theme: 'Aventura',
-    date: new Date('2022-07-16')
-   },
-    {
-    name: 'Sessao FERAS',
-    system: 'D&D 5e',
-    theme: 'Aventura',
-    date: new Date('2022-07-16')
+    date: new Date('2022-07-16'),
+    description: ''
    },
   ]
 
---
+++ src/app/group.model.ts
@@ -0,0 +1,9 @@
+export class Group {
+  constructor(
+    public title: string,
+    public system: string,
+    public theme: string,
+    public date: Date,
+    public description: string,
+  ) {}
+}
